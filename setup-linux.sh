9f9de816822a0405f6564718965ddd964fc3680a
Nichoals Pipitone <npip99@gmail.com>
2020-05-11 12:01:47 -0500

    working setup linux sh

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+sudo echo login
+
 if ! [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
 	echo "Usage: ./setup-linux.sh [IP ADDRESS]"
 	exit
 fi
 
 scp -r linux Fractal@$1:~/linux
-/bin/bash linux/cloud-0.sh | ssh Fractal@$1 /bin/bash
+echo "cd ~/linux; /bin/bash linux/cloud-0.sh" | ssh Fractal@$1 /bin/bash
 sleep 15
-/bin/bash linux/cloud-1.sh | ssh Fractal@$1 /bin/bash
+echo "cd ~/linux; /bin/bash linux/cloud-1.sh" | ssh Fractal@$1 /bin/bash
 ssh-keygen -f "/home/npip99/.ssh/known_hosts" -R "$1"
 
