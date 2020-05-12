7584885d3407dd7132582411ade72f741fa99317
Nichoals Pipitone <npip99@gmail.com>
2020-05-12 16:48:17 -0500

    linux setup

--- setup-linux.sh
+++ setup-linux.sh
@@ -7,9 +7,7 @@ if ! [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
 	exit
 fi
 
-echo "rm -rf ~/fractal-setup" | ssh Fractal@$1 /bin/bash
-scp -r linux Fractal@$1:~/linux
-echo "cd ~/fractal-setup; /bin/bash linux/cloud-0.sh" | ssh Fractal@$1 /bin/bash
+echo "mkdir ~/fractal-setup; cd ~/fractal-setup; /bin/bash linux/cloud-0.sh" | ssh Fractal@$1 /bin/bash
 sleep 30
 echo "cd ~/fractal-setup; /bin/bash linux/cloud-1.sh" | ssh Fractal@$1 /bin/bash
 ssh-keygen -f "/home/npip99/.ssh/known_hosts" -R "$1"
