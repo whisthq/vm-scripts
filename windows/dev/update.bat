d47ec1fcdb9234476dfe3df90cd73200ebae1dff
philippemnoel <philippe_noel@college.harvard.edu>
2020-05-26 21:33:16 -0400

    adding some S3 bucket files

---
+++ windows/dev/update.bat
@@ -0,0 +1,32 @@
+cd %temp%
+rmdir /S/Q fractal-protocol
+mkdir fractal-protocol
+cd fractal-protocol
+echo %1%
+
+set file=%1%
+
+IF NOT "%~1" == "" (
+  set file=%1%/
+)
+
+powershell -command "iwr -outf version https://fractal-cloud-setup-s3bucket.s3.amazonaws.com/%file%version"
+fc /b version "C:\Program Files\Fractal\version" > nul
+
+if errorlevel 1 (
+  move /Y version "C:\Program Files\Fractal"
+
+  powershell -command "iwr -outf FractalServer.exe https://fractal-cloud-setup-s3bucket.s3.amazonaws.com/%file%FractalServer.exe"
+
+  
+  net stop Fractal
+  taskkill /IM "FractalService.exe" /F
+  taskkill /IM "FractalServer.exe" /F
+  timeout /t 1
+  del /F "C:\Program Files\Fractal\FractalServer.exe"
+  timeout /t 1
+  move /Y FractalServer.exe "C:\Program Files\Fractal"
+  sc.exe Start "Fractal"
+)
+
+cd C:\Program Files\Fractal
