4a5a3c3bb1f7566780ec9e720204cd6ce15aa6d2
Avery Lamp <averylamp@gmail.com>
2020-05-28 10:32:33 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-aws s3 cp linux/utils.sh s3://arn:aws:s3:us-east-1:747391415460:accesspoint/fractal-cloud-setup/utils.sh
-
+aws s3 cp linux/utils.sh s3://arn:aws:s3:us-east-1:747391415460:accesspoint/fractal-cloud-setup/utils.sh && \
 curl -X POST --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"fractal-bot\", \"text\": \"setup-scripts utils.sh updated in AWS S3\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/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
 
