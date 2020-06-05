87aac307fb1ca9d46e0c8466aac89c9a50f3cd34
philippemnoel <philippe_noel@college.harvard.edu>
2020-06-04 22:22:19 -0400

    modify fractal linux service to re-download protocol if not present

--- linux/FractalServer.sh
+++ linux/FractalServer.sh
@@ -9,6 +9,6 @@ export XAUTHORITY=$(systemctl --user show-environment | awk -F = '/XAUTHORITY/{p
 # if error --> 2 == No such file or directory
 if [ "$?" == "2" ]; then
     echo "Downloading Fractal Server"
-    sudo wget -qO /usr/share/fractal/FractalServer "https://fractal-cloud-setup-s3bucket.s3.amazonaws.com/$1/FractalServer"
+    sudo wget -qO /usr/share/fractal/FractalServer "https://fractal-cloud-setup-s3bucket.s3.amazonaws.com/master/FractalServer"
     sudo chmod g+x /usr/share/fractal/FractalServer # make FractalServer executable
 fi
