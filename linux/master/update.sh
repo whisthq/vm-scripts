08db8a01bb8c6a24c5a7f1b7cf91af7f7fd3d0c6
philippemnoel <philippe_noel@college.harvard.edu>
2020-06-20 11:42:59 -0400

    added auto-updating to FFmpeg libs, service and service helper script!

--- linux/master/update.sh
+++ linux/master/update.sh
@@ -13,11 +13,20 @@ if [ "$v1" -ne "$v2" ]
 then
   curl https://fractal-cloud-setup-s3bucket.s3.amazonaws.com/${branch}/Linux/FractalServer > FractalServer
   curl https://fractal-cloud-setup-s3bucket.s3.amazonaws.com/${branch}/Linux/update.sh > update.sh
-
+  curl https://fractal-cloud-setup-s3bucket.s3.amazonaws.com/${branch}/Linux/fractal.service > fractal.service
+  curl https://fractal-cloud-setup-s3bucket.s3.amazonaws.com/${branch}/Linux/FractalServer.sh > FractalServer.sh
+  curl https://fractal-protocol-shared-libs.s3.amazonaws.com/shared-libs.tar.gz | tar -xz
+  
   sudo -u fractal DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$FRACTAL_UID/bus systemctl --user stop fractal
   pkill FractalServer
   cp FractalServer /usr/share/fractal/FractalServer
   cp update.sh /usr/share/fractal/update.sh
   cp version /usr/share/version
+  cp FractalServer.sh /usr/share/fractal/FractalServer.sh
+  cp fractal.service /usr/share/fractal/fractal.service
+  cp share/64/Linux/* /usr/share/fractal/
+
   sudo -u fractal DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$FRACTAL_UID/bus systemctl --user start fractal
 fi
+
+rm -rf /tmp/fractal-protocol
